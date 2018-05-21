84eb335b70a0533ac2c80c2690e3c9fe1bfaed31
ziv aharoni <ziv.aharoni.24@gmail.com>
2018-05-21 18:24:57 +0300

    bug fixes

--- pytorch_impl/data.py
+++ pytorch_impl/data.py
@@ -33,6 +33,7 @@ class Corpus(object):
 
     def tokenize(self, path):
         """Tokenizes a text file."""
+        print(path)
         assert os.path.exists(path)
         # Add words to the dictionary
         with open(path, 'r', encoding='utf-8') as f:
